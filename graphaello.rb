e8198803c8197daa3e822ee1e8f71d981f44c86c
Mathias Quintero <me@quintero.io>
2019-12-18 15:12:33 +0100

    updating graphaello

--- graphaello.rb
+++ graphaello.rb
@@ -2,7 +2,7 @@ class Graphaello < Formula
   desc "Manages the state of the GraphQL Integrations in your project"
   homepage "https://bitbucket.ase.in.tum.de/projects/MMQ/repos/graphaello"
   url "https://bitbucket.ase.in.tum.de/scm/mmq/graphaello.git",
-  	:tag => "0.1.0"
+  	:tag => "0.1.1"
   head "https://bitbucket.ase.in.tum.de/scm/mmq/graphaello.git"
   depends_on :xcode => ["11.0", :build]
 
