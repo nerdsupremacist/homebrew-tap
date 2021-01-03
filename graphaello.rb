9ce00497787dcd3e11b3c91d6f48e2af5f13b202
Mathias Quintero <me@quintero.io>
2021-01-03 18:04:26 +0100

    Update graphaello.rb

--- graphaello.rb
+++ graphaello.rb
@@ -3,9 +3,10 @@ class Graphaello < Formula
   homepage "https://github.com/nerdsupremacist/Graphaello"
   version "0.1.0-alpha.15.2"
   url "https://github.com/nerdsupremacist/Graphaello/releases/download/0.1.0-alpha.15.2/release.tar.gz", :using => :curl
+  sha256 "84dc5fb14a61075bceaf6fe065bf961f61a69ebef3691cd950c14eed297f536a"
 
   def install
-    system "tar", "xf", "release.tar.gz" 
-    system "mv", "-v", "./release/*", :prefix
+    system "tar", "xf", "01.tar" 
+    system "mv", "-v", "./01/*", :prefix
   end
 end
