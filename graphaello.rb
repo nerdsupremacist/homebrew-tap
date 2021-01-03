40aa971f3c00482bf7db59a5dd0abe58726c45da
Mathias Quintero <me@quintero.io>
2021-01-03 16:36:50 +0100

    Update graphaello.rb

--- graphaello.rb
+++ graphaello.rb
@@ -2,7 +2,7 @@ class Graphaello < Formula
   desc "Manages the state of the GraphQL Integrations in your project"
   homepage "https://github.com/nerdsupremacist/Graphaello"
   url "https://github.com/nerdsupremacist/Graphaello.git",
-  	:tag => "0.1.0-alpha.14"
+  	:tag => "0.1.0-alpha.15"
   head "https://github.com/nerdsupremacist/Graphaello.git"
   depends_on :xcode => ["12.0", :build]
 
