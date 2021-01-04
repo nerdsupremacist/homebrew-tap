aac09d353c2d04917cbd54b4e5eae3934107711d
Mathias Quintero <me@quintero.io>
2021-01-04 08:26:38 +0100

    graphaello 0.1.0-alpha.17
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/graphaello.rb
+++ Formula/graphaello.rb
@@ -1,9 +1,9 @@
 class Graphaello < Formula
   desc "Manages the state of the GraphQL Integrations in your project"
   homepage "https://github.com/nerdsupremacist/Graphaello"
-  version "0.1.0-alpha.16"
-  url "https://github.com/nerdsupremacist/Graphaello/releases/download/0.1.0-alpha.16/release.tar.gz", :using => :curl
-  sha256 "dcb1c92c2cb09e143caec57a83608ea2a163245d1a653a8a5871399894ba21da"
+  version "0.1.0-alpha.17"
+  url "https://github.com/nerdsupremacist/Graphaello/releases/download/0.1.0-alpha.17/release.tar.gz", :using => :curl
+  sha256 "c5c9e21859223978527fa185e4d35b3d49d96c7ab7215a8d2c95a85e6017d378"
 
   def install
     system "cp", "-R", ".", prefix
