f6a1ee7b1963d245dd147dedb52b12e3e34243c3
Mathias Quintero <me@quintero.io>
2022-07-02 14:28:34 +0100

    Update gyb.rb

--- Formula/gyb.rb
+++ Formula/gyb.rb
@@ -8,13 +8,13 @@ class Gyb < Formula
   homepage "https://nshipster.com/swift-gyb/"
   url "https://raw.githubusercontent.com/apple/swift/3dfc40898cdf6980bac4f6987e57719346833470/utils/gyb.py"
   version "2022-04-13"
-  sha256 "5965040b4c04528191d392d57282b1c5970fb8c3fedf1fb64bf6412173d6ac72"
+  sha256 "c08f7594f84acf26ebc3f329fd1c79709102c22c37ccbea0553b99763059b638"
 
   depends_on xcode: ["10.0", :build]
 
   resource "gyb.py" do
     url "https://raw.githubusercontent.com/apple/swift/3dfc40898cdf6980bac4f6987e57719346833470/utils/gyb.py"
-    sha256 "5965040b4c04528191d392d57282b1c5970fb8c3fedf1fb64bf6412173d6ac72"
+    sha256 "c08f7594f84acf26ebc3f329fd1c79709102c22c37ccbea0553b99763059b638"
     version "2022-04-13"
   end
   depends_on "python@3" if MacOS.version <= :snow_leopard
